c0589219679fcb16402b0261b5d798b2a7127b07
David Lozada <juand_gonzalez20@soy.sena.edu.co>
2024-08-06 13:08:23 -0500

    subir java

---
+++ Servipet/Servipet/src/main/java/com/ServiPet/Servipet/ServipetApplication.java
@@ -0,0 +1,13 @@
+package com.ServiPet.Servipet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServipetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServipetApplication.class, args);
+	}
+
+}
---
+++ Servipet/Servipet/src/test/java/com/ServiPet/Servipet/ServipetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ServiPet.Servipet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServipetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
